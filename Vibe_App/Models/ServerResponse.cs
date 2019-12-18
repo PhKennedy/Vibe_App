db8d458274b2426a39c97c9a10a4dc76848d1725
Philipi Magalhães <philipimagalhaes@gmail.com>
2019-12-18 13:06:34 -0300

    Create ServerResponse.cs

---
+++ Vibe_App/Models/ServerResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vibe_App.Models
+{
+   public class Token
+    {
+        public string Chave { get; set; }
+    }
+}
